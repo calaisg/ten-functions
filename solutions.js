3c0c64e959d4a4803c309a4a359ab28b6ba467c8
Calais Galbraith <calaisgalbraith@gmail.com>
2021-06-01 14:29:51 -0500

    Ten solutions solutions

--- solutions.js
+++ solutions.js
@@ -9,14 +9,100 @@
  * If you don't know what an iffe is, don't worry about it :)
  *
  * This file is for defining the following functions. See README.md for more detail.
- * isTrue
- * isFalse
- * not
- * addOne
- * isEven
- * isIdentical
- * isEqual
- * or
- * and
- * concat
+ * isTrue // DONE
+ * isFalse // DONE
+ * not // DONE
+ * addOne //DONE
+ * isEven // DONE
+ * isIdentical // DONE
+ * isEqual // DONE
+ * or // DONE
+ * and // DONE
+ * concat // DONE
  */
+
+function isTrue(value)
+{
+    if(value === true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+function isFalse(value)
+{
+    if (value === false)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function not(value)
+{
+    return !value;
+}
+
+function addOne(value)
+{
+    return (Number(value) + 1);
+}
+
+function isEven(value)
+{
+    if (Number(value) % 2 === 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function isIdentical(value_one, value_two)
+{
+    if (value_one === value_two)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function isEqual(value_one, value_two)
+{
+    if(value_one == value_two)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function or(value_one, value_two)
+{
+    return value_one || value_two;
+}
+
+function and(value_one, value_two)
+{
+    return value_one && value_two;
+}
+
+function concat(value_one, value_two)
+{
+    return value_one.toString() + value_two.toString();
+}
